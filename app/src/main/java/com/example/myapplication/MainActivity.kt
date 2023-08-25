993c283caccbbb7648b3cb71eb68da8c152b28dd
jinxiang.wu <jinxiang.wu@yunzhanghu.com>
2023-08-25 17:55:02 +0800

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.kailaisi.test.R
 
@@ -8,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startActivity(Intent(this, FlutterEmbeddingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MyApp.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+import androidx.multidex.MultiDexApplication
+
+class MyApp : MultiDexApplication() {
+}
